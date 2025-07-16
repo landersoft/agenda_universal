7cead752fceb2568514f7e94640c09c5727ad585
landersoft <landersoft@gmail.com>
2025-07-15 23:27:33 -0400

    agregamos nueva ruta/endpoint

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,10 @@ def index():
     collection = current_app.db["mensajes"]
     count = collection.count_documents({})
     return jsonify({"mensaje": "Hola Flask + Mongo", "total_mensajes": count})
+
+
+@bp.route('/test-db')
+def test_db():
+    collection = current_app.db["profesionales"]  # usa la colección que desees
+    docs = list(collection.find({}, {"_id": 0}))  # sin mostrar _id
+    return jsonify(docs)
