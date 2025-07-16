7e1ee9c22ddb6f9a5c4eaaef4a0ef2bacc7c33dc
landersoft <landersoft@gmail.com>
2025-07-16 12:29:49 -0400

    formateado con black lint

--- app/routes.py
+++ app/routes.py
@@ -5,18 +5,19 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    #collection = current_app.db["mensajes"]
-    #count = collection.count_documents({})
+    # collection = current_app.db["mensajes"]
+    # count = collection.count_documents({})
     return jsonify({"mensaje": "Hola Flask + Mongo"})
 
 
 @bp.route("/test-db", methods=["GET"])
 def test_db_get():
     db = current_app.db
-    #collection = current_app.db["profesionales"]  # usa la colección que desees
+    # collection = current_app.db["profesionales"]  # usa la colección que desees
     documentos = list(db.profesionales.find({}, {"_id": 0}))  # sin mostrar _id
-    #docs = list(collection.find({}, {"_id": 0}))  # sin mostrar _id
+    # docs = list(collection.find({}, {"_id": 0}))  # sin mostrar _id
     return jsonify(documentos), 200
+
 
 @bp.route("/test-db", methods=["POST"])
 def test_db_post():
@@ -25,11 +26,13 @@ def test_db_post():
     if not data:
         nuevo_documento = {"nombre": "Anibal", "especialidad": "General"}
         result = db.profesionales.insert_one(nuevo_documento)
-        return jsonify({"mensaje": "Documento insertado", "id": str(result.inserted_id)}), 200
-        
-    
+        return (
+            jsonify({"mensaje": "Documento insertado", "id": str(result.inserted_id)}),
+            200,
+        )
+
     result = db.profesionales.insert_one(data)
-    return jsonify({
-        "mensaje": "Documento insertado",
-        "id": str(result.inserted_id)
-        }), 201
+    return (
+        jsonify({"mensaje": "Documento insertado", "id": str(result.inserted_id)}),
+        201,
+    )
