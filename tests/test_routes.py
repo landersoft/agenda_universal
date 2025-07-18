31111486e6d69b9cba8c7acede2bdaa70544cef1
landersoft <landersoft@gmail.com>
2025-07-18 16:38:21 -0400

    agrega endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,15 @@ from app import create_app
 
 def test_index():
     app = create_app()
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+
+def test_insert_db():
+    app = create_app()
+    with app.test_client() as client:
+        response = client.get("/insert-db")
+        assert response.status_code == 201
+        data = response.get_json()
+        assert "inserted" in data
