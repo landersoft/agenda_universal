a48f88e8239a39c2a727fa79b88437e763086352
landersoft <landersoft@gmail.com>
2025-08-02 10:55:03 -0400

    cambios de black

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,12 +8,20 @@ def test_index():
         response = client.get("/")
         assert response.status_code == 200
 
+
 def test_insert_db():
     app = create_app()
     with app.app_context():
         db = current_app.db
         # Insertamos datos de prueba
-        db.especialidades.insert_one({"nombre": "Cardiología","codigo": "CAR001", "descripcion": "Especialidad del corazón", "taxonomia": ["corazón", "cardiología"]})
+        db.especialidades.insert_one(
+            {
+                "nombre": "Cardiología",
+                "codigo": "CAR001",
+                "descripcion": "Especialidad del corazón",
+                "taxonomia": ["corazón", "cardiología"],
+            }
+        )
 
 
 def test_especialidades():
