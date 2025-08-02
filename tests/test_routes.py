4acbe7ee53c2242a1245dfaab899fdd04008577c
landersoft <landersoft@gmail.com>
2025-08-02 10:50:37 -0400

    test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,5 @@
 from app import create_app
+from flask import current_app
 
 
 def test_index():
@@ -7,8 +8,15 @@ def test_index():
         response = client.get("/")
         assert response.status_code == 200
 
-
 def test_insert_db():
+    app = create_app()
+    with app.app_context():
+        db = current_app.db
+        # Insertamos datos de prueba
+        db.especialidades.insert_one({"nombre": "Cardiología","codigo": "CAR001", "descripcion": "Especialidad del corazón", "taxonomia": ["corazón", "cardiología"]})
+
+
+def test_especialidades():
     app = create_app()
     with app.test_client() as client:
         response = client.get("/especialidades")
